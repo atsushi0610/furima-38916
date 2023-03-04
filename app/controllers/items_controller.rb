dcb88fe49148979ff0bd83c8e6dd9f9d2e6763e4
atsushi0610 <kogure.bbc@gmail.com>
2023-03-04 21:22:42 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,26 @@ class ItemsController < ApplicationController
   #before_action :move_to_index, except: [:index, :show]
 
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:title,:description,:category_id,:condition_id,:postage_id,:region_id,:duration_id,:price).merge(user_id: current_user.id)
+  end
+
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context '商品が出品できる場合' do
+      it 'title、description、category_id、condition_id、postage_id、region_id、duration_id、price、imageが存在しuserが紐づていれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品が出品できない場合' do
+      it 'imageが空では保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'titleが空だと保存できない' do
+        @item.title = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'descriptionが紐付いていないと保存できない' do
+        @item.description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'category_idが空では保存できない' do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idが紐付いていないと保存できない' do
+        @item.condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'postage_idが紐付いていないと保存できない' do
+        @item.postage_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
+      end
+      it 'region_idが空では保存できない' do
+        @item.region_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region can't be blank")
+      end
+      it 'duration_idが空では保存できない' do
+        @item.duration_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Duration can't be blank")
+      end
+      it 'priceが空では保存できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが全角では保存できない' do
+        @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが¥300未満では保存できない' do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが¥9,999,999を超えると保存できない' do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it "ユーザーが紐付いていないとツイートは保存できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
+end
