4329137467a3a13acf571a7fab725e548f430705
atsushi0610 <kogure.bbc@gmail.com>
2023-03-05 14:25:51 +0900

    コントローラーとテストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -97,7 +97,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
-      it "ユーザーが紐付いていないとツイートは保存できない" do
+      it "ユーザーが紐付いていないと商品は保存できない" do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
