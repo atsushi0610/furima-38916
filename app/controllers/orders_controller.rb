196fb0e0292c4f3cfdd67e653987fb6c4e0e9401
atsushi0610 <kogure.bbc@gmail.com>
2023-03-08 20:05:18 +0900

    Formオブジェクトパターンの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,8 +6,24 @@ class OrdersController < ApplicationController
     if current_user == @item.user
       redirect_to root_path
     end
+
+    @order_shipping = OrderShipping.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:post_code, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
