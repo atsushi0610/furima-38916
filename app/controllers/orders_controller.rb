0c70836849c43735f07e437eb0ecaab650bd45ff
atsushi0610 <kogure.bbc@gmail.com>
2023-03-08 16:44:57 +0900

    コントローラーとモデルの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def create
   end
 end
