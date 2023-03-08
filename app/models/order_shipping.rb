c099d7576a9b268907f3432951bc48892e98f3e4
atsushi0610 <kogure.bbc@gmail.com>
2023-03-08 15:34:50 +0900

    Formオブジェクトの作成

---
+++ app/models/order_shipping.rb
@@ -0,0 +1,19 @@
+class OrderShipping
+  include ActiveModel::Model
+  attr_accessor :post_code, :region_id, :city, :block, :building :user_id, :item_id
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :block
+    validates :phone_number, format: {with: /\A0\d{9,10}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :user_id
+    validates :item_id
+  end
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank" } 
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Shipping.create(postal_code: postal_code, region_id: region_id, city: city, block: block, building: building, phone_number: phone_number, user_id: user.id, , donation_id: donation.id)
+  end
+end
