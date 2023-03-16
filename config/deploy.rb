617272dfb1a7b4295be52bfb2fc47e4953436a0c
atsushi0610 <kogure.bbc@gmail.com>
2023-03-16 15:24:11 +0900

    自動デプロイの設定

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.17.2'
+
+set :application, 'furima-38916'
+
+set :repo_url,  'git@github.com:atsushi0610/furima-38916.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/kogure.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
